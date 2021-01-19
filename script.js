658eba79a0b3c3c18c01a1d691d4b06f8687aac8
T-98 <dkhare@ucsc.edu>
2021-01-18 21:07:19 -0800

    MVP

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-var tasks = {};
+var tasks = [];
+
+var date = moment().format('dddd, MMMM Do');
+document.getElementById("currentDay").innerHTML = date;
 
 var createCalendar = function(taskText) {
 
@@ -7,13 +10,17 @@ var createCalendar = function(taskText) {
         if (i > 12) var time = (i - 12) + "PM";
         else var time = i + "AM";
 
+        if (i === moment().hours()) var timeClass = "present";
+        else if (i > moment().hours()) var timeClass = "future";
+        else var timeClass = "past";
+
         var taskRow = $("<div>").addClass("row-container row align-items-start");
 
-        var taskCol1 = $("<div>").addClass("hour col-1 d-flex").attr("style", "border-right: 2px solid black; justify-content: flex-end;");
+        var taskCol1 = $("<div>").addClass("hour col-1 d-flex").attr("style", "justify-content: flex-end;");
         taskCol1.text(time);
 
-        var taskCol2 = $("<div>").addClass("row col-9 d-flex").attr("style", "background-color: grey;");
-        var taskP = $("<p>").addClass("m-0").text(taskText);
+        var taskCol2 = $("<div>").addClass("taskbox row col-9 d-flex " + timeClass);
+        var taskP = $("<p>").addClass("m-0").text(tasks[i - 9]);
         taskCol2.append(taskP);
 
         var taskCol3 = $("<div>").addClass("row saveBtn col-1 d-flex btn btn-primary");
@@ -26,14 +33,6 @@ var createCalendar = function(taskText) {
 
         $(".container").append(taskRow);
     }
-    /*
-    var taskList = $("<li>").addClass("list-group-item");
-    var taskP = $("<p>")
-        .addClass("m-1")
-        .text(taskText);
-
-    $("list").append(taskList);
-    $("list-group-item").append(taskP); */
 }
 
 var cleanCalendar = function() {
@@ -47,20 +46,49 @@ var saveTasks = function() {
 var loadTasks = function() {
     tasks = JSON.parse(localStorage.getItem("tasks"));
 
-    if (tasks.length == 0) {
-        tasks = [];
-    }
-
-    for (var i = 0; i < tasks.length; ++i) {
-        cleanCalendar();
-        createCalendar(tasks[i]);
-    }
-
 }
 
-saveTasks();
+cleanCalendar();
 loadTasks();
 createCalendar();
 
-//build the li in ht
-//moment().hours()
+var currentSelection;
+
+$(".taskbox").on("click", function() {
+    var text = $(this).text().trim();
+
+    var textInput = $("<textarea>")
+        .addClass("textarea")
+        .val(text);
+
+    $(this).children("p").replaceWith(textInput);
+    textInput.trigger("focus");
+    currentSelection = this;
+    console.log("click");
+
+
+});
+
+$(".taskbox").on("blur", "textarea", function() {
+    var text = $(this).val().trim();
+
+    var taskP = $("<p>")
+        .addClass("m-0")
+        .text(text);
+
+    $(this).replaceWith(taskP);
+
+    console.log("blur");
+});
+
+$(".saveBtn").on("click", function() {
+    var text = $(this).closest(".row-container").children(".taskbox").children("p").text().trim();
+
+    var index = $(this)
+        .closest(".row-container")
+        .index();
+
+    tasks[index] = text;
+
+    saveTasks();
+});
